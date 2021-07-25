61f47a338f7f082998dd8dc383571707a7612bf7
barly.djaja <barlydjaja@gmail.com>
2021-07-26 00:42:39 +0700

    add progress loading on blog

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
+import Loader from "react-loader-spinner";
 
 const Blog = () => {
   return (
-    <div>
-      <h1> this is the blog page</h1>
+    <div
+      className={"d-flex vh-100 text-center text-white"}
+      style={{ backgroundColor: "#FFA1A9" }}
+    >
+      <div
+        className={
+          styles.coverContainer + " d-flex w-100 h-100 p-3 mx-auto flex-column"
+        }
+      >
+        <Header />
+        <main style={{ marginTop: "24px" }}>
+          <div className="flex-justify-center">
+            <Loader type="Hearts" color="#ffff60" height={75} width={75} />
+            <Loader type="Hearts" color="#ffff60" height={75} width={75} />
+          </div>
+          <h1>On Progress</h1>
+        </main>
+      </div>
     </div>
   );
 };
