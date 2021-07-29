c2953fe323e69caec0ac38be1f1703336b29b33b
barly.djaja <barlydjaja@gmail.com>
2021-07-29 14:34:43 +0700

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Router from "next/router"
+import withGA from "next-ga"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default withGA("UA-145060279-2", Router)(MyApp);
